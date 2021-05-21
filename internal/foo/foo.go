74fc7c5153ec6c6d5448c4b945177174f6d6f3ac
Yuki Ito <mrno110y@gmail.com>
2021-05-21 15:10:02 +0900

    add internal/foo

---
+++ internal/foo/foo.go
@@ -0,0 +1,5 @@
+package foo
+
+func Sum(x, y int) int {
+	return x + y
+}
---
+++ internal/foo/foo_test.go
@@ -0,0 +1,33 @@
+package foo_test
+
+import (
+	"testing"
+
+	"github.com/110y/build-buddy-example/internal/foo"
+)
+
+func TestSum(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		x        int
+		y        int
+		expected int
+	}{
+		"1 + 1": {
+			x:        1,
+			y:        1,
+			expected: 2,
+		},
+	}
+
+	for name, test := range tests {
+		test := test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			if expected, actual := test.expected, foo.Sum(test.x, test.y); actual != expected {
+				t.Errorf("expected: %d, actual: %d", expected, actual)
+			}
+		})
+	}
+}
